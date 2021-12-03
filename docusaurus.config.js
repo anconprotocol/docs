56166a635afea0da8fdd9cc8114af51c92c7717d
Kendall Kant <kantkendall@gmail.com>
2021-12-03 15:38:36 -0500

    revert baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Ancon Protocol Docs',
   tagline: 'Ancon Protocol Documentation',
   url: 'https://electronic-signatures-industries.github.io/',
-  baseUrl: '/docs/',
+  baseUrl: '/ancon-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
